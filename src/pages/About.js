6798e7731c38567507cff16370a1897595e80171
zinx110 <tasdid110@gmail.com>
2021-11-18 01:06:21 +0600

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+
+      <p>
+        Lorem lanjfoanaom onl oien ljhpae4 phagjop pj a Lorem lanjfoanaom onl
+        oien ljhpae4 phagjop pj a Lorem lanjfoanaom onl oien ljhpae4 phagjop pj
+        a Lorem lanjfoanaom onl oien ljhpae4 phagjop pj a Lorem lanjfoanaom onl
+        oien ljhpae4 phagjop pj a
+      </p>
+    </section>
+  );
+};
+
+export default About;
